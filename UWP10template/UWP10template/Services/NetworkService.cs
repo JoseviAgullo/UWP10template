7d03b36ff8b7ab49802f84a75a367d20e9fc74c1
mapaux <josevi.agullo@gmail.com>
2015-11-02 22:58:59 +0000

    Added more services

---
+++ UWP10template/UWP10template/Services/NetworkService.cs
@@ -0,0 +1,14 @@
+﻿namespace UWP10template.Services
+{
+    using Windows.Networking.Connectivity;
+
+    public class NetworkService
+    {
+        public bool IsOnline()
+        {
+            var internetProfile = NetworkInformation.GetInternetConnectionProfile();
+            return internetProfile != null &&
+                internetProfile.GetNetworkConnectivityLevel() == NetworkConnectivityLevel.InternetAccess;
+        }
+    }
+}
---
+++ UWP10template/UWP10template/Services/StorageService.cs
@@ -0,0 +1,145 @@
+﻿namespace UWP10template.Services
+{
+    using System;
+    using System.Threading.Tasks;
+    using Windows.Storage;
+
+    public class StorageService
+    {
+        public void saveSetting(string setting, string value)
+        {
+            var roamingData = Windows.Storage.ApplicationData.Current.RoamingSettings;
+            roamingData.Values[setting] = value;
+        }
+
+        public void deleteSetting(string setting)
+        {
+            var roamingData = Windows.Storage.ApplicationData.Current.RoamingSettings;
+            roamingData.Values[setting] = null;
+        }
+
+        public void clearSettings()
+        {
+            var roamingData = Windows.Storage.ApplicationData.Current.RoamingSettings;
+            roamingData.Values.Clear();
+        }
+
+        public async Task saveTextFile(StorageFolder folder, string data, string key = null)
+        {
+            StorageFile myFile;
+            if (key == null)
+            {
+                myFile = await folder.CreateFileAsync("myData.txt", CreationCollisionOption.OpenIfExists);
+            }
+            else
+            {
+                myFile = await folder.CreateFileAsync(key + ".txt", CreationCollisionOption.OpenIfExists);
+            }
+
+            await FileIO.WriteTextAsync(myFile, data);
+        }
+
+        public async Task saveAppendTextFile(StorageFolder folder, string data, string key = null)
+        {
+            StorageFile myFile;
+            if (key == null)
+            {
+                myFile = await folder.CreateFileAsync("myData.txt", CreationCollisionOption.OpenIfExists);
+            }
+            else
+            {
+                myFile = await folder.CreateFileAsync(key + ".txt", CreationCollisionOption.OpenIfExists);
+            }
+            await FileIO.AppendTextAsync(myFile, data);
+            await FileIO.AppendTextAsync(myFile, Environment.NewLine);
+        }
+
+        public async Task<string> loadTextFile(StorageFolder folder, string key = null)
+        {
+            StorageFile myFile;
+            try
+            {
+                if (key == null)
+                {
+                    myFile = await folder.GetFileAsync("myData.txt");
+                }
+                else
+                {
+                    myFile = await folder.GetFileAsync(key + ".txt");
+                }
+            }
+            catch
+            {
+                myFile = null;
+            }
+
+            string text = "";
+            if (myFile != null)
+            {
+                text = await FileIO.ReadTextAsync(myFile);
+            }
+
+            return text;
+        }
+
+        public async Task clearTextFile(StorageFolder folder, string key = null)
+        {
+            StorageFile myFile;
+            if (key == null)
+            {
+                myFile = await folder.CreateFileAsync("myData.txt", CreationCollisionOption.OpenIfExists);
+            }
+            else
+            {
+                myFile = await folder.CreateFileAsync(key + ".txt", CreationCollisionOption.OpenIfExists);
+            }
+
+            await FileIO.WriteTextAsync(myFile, "");
+        }
+
+        public ApplicationDataContainer getRoamingSettings()
+        {
+            return ApplicationData.Current.RoamingSettings;
+        }
+
+        public StorageFolder getLocalFolder()
+        {
+            return ApplicationData.Current.LocalFolder;
+        }
+
+        public StorageFolder getTempFolder()
+        {
+            return ApplicationData.Current.TemporaryFolder;
+        }
+
+        //If device does't have SD card, returns LocalFolder
+        public async Task<StorageFolder> getSdFolder()
+        {
+            StorageFolder firstCard = getLocalFolder();
+            var devices = Windows.Storage.KnownFolders.RemovableDevices;
+            var sdCards = await devices.GetFoldersAsync();
+            firstCard = sdCards[0];
+            return firstCard;
+        }
+
+        public StorageFolder getPicturesKnownFolders()
+        {
+            return KnownFolders.PicturesLibrary;
+        }
+
+        public StorageFolder getVideosKnownFolders()
+        {
+            return KnownFolders.VideosLibrary;
+        }
+
+        public StorageFolder getMusicKnownFolders()
+        {
+            return KnownFolders.MusicLibrary;
+        }
+
+        public StorageFolder getDocumentsKnownFolders()
+        {
+            return KnownFolders.DocumentsLibrary;
+        }
+    }
+}
