bfca103727891a49fc84283cf14ca7a10bc96f2a
mapaux <josevi.agullo@gmail.com>
2015-11-02 23:19:16 +0000

    Hamburguer menu v1.0

--- UWP10template/UWP10template/Views/MainPage.xaml.cs
+++ UWP10template/UWP10template/Views/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace UWP10template.Views
         {
             this.InitializeComponent();
         }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
     }
 }
