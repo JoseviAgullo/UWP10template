ca3681d4a73383bcabca9b1654fbfb9df77886d2
mapaux <josevi.agullo@gmail.com>
2015-11-03 14:11:08 +0000

    Hamburguer menu v2.0.2 (Auto hide)

---
+++ UWP10template/UWP10template/ViewModels/ShellViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using UWP10template.Models;
+using UWP10template.Services;
+using UWP10template.Views;
+
+namespace UWP10template.ViewModels
+{
+    public class ShellViewModel : ObservableObject
+    {
+        private static ObservableCollection<MenuItem> menu = new ObservableCollection<MenuItem>();
+
+        public ObservableCollection<MenuItem> Menu
+        {
+            get { return menu; }
+        }
+
+        public ShellViewModel(NavigationService navigationService)
+        {
+            Menu.Add(new MenuItem() { Glyph = "", Text = "Main Page", NavigationDestination = typeof(MainPage) });
+            Menu.Add(new MenuItem() { Glyph = "", Text = "About Page", NavigationDestination = typeof(AboutPage) });
+            Menu.Add(new MenuItem() { Glyph = "", Text = "Third Page", NavigationDestination = null });
+        }
+    }
+}
---
+++ UWP10template/UWP10template/Views/Shell.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using UWP10template.Models;
+using UWP10template.ViewModels;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWP10template.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Shell : Page
+    {
+        public Shell()
+        {
+            this.InitializeComponent();
+
+            // Navigate to the first page (optionally)
+            var type = (DataContext as ShellViewModel).Menu.First().NavigationDestination;
+            SplitViewFrame.Navigate(type);
+        }
+
+        private void Menu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0)
+            {
+                var menuItem = e.AddedItems.First() as MenuItem;
+                if (menuItem.IsNavigation)
+                {
+                    SplitViewFrame.Navigate(menuItem.NavigationDestination);
+                }
+                else
+                {
+                    menuItem.Command.Execute(null);
+                }
+            }
+            ChangeSplitViewState();
+        }
+
+        private void SplitViewOpener_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            if (e.Cumulative.Translation.X > 50)
+            {
+                MySplitView.IsPaneOpen = true;
+            }
+        }
+
+        private void SplitViewPane_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            if (e.Cumulative.Translation.X < -50)
+            {
+                MySplitView.IsPaneOpen = false;
+            }
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeSplitViewState();
+        }
+
+        private void ChangeSplitViewState()
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+    }
+}
